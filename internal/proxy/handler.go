7e39f4236f143a4ece841fad98ec26572657a169
Bastian Dietrich <git@0x2321.de>
2025-08-20 22:44:48 +0200

    overwrite sender address with client-provided header

--- internal/proxy/handler.go
+++ internal/proxy/handler.go
@@ -56,6 +56,7 @@ func mailHandler(c *client.Client, overwriteFrom string) func(net.Addr, string, 
 		})
 
 		// Overwrite sender address
+		from = in.Header.Get(string(client.HeaderFrom))
 		if overwriteFrom != "" {
 			from = overwriteFrom
 		}
